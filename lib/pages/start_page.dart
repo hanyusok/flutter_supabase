bce476bfcb5513ebc5cb2d0b2ea8e2ae49dea565
hanyusok <hanyusok@gmail.com>
2024-06-18 07:54:09 +0900

    google login

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -1,3 +1,4 @@
+import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
 import "package:supabase_flutter/supabase_flutter.dart";
 
@@ -15,6 +16,7 @@ class _StartPageState extends State<StartPage> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
+  bool _googleSignInLoading = false;
 
   @override
   void dispose() {
@@ -38,7 +40,93 @@ class _StartPageState extends State<StartPage> {
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
                     Image.network(
-                        "https://seeklogo.com/images/S/supabase-logo-DCC676FFE2-seeklogo.com.png")
+                        "https://seeklogo.com/images/S/supabase-logo-DCC676FFE2-seeklogo.com.png", height: 150,),
+                    const SizedBox(height: 25,),
+                    TextFormField(
+                      validator: (value){
+                        if (value == null || value.isEmpty) {
+                          return "Field is required!";
+                        }
+                        return null;
+                      },
+                      controller: _emailController,
+                      decoration: const InputDecoration(label: Text("Email")),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    TextFormField(
+                      validator: (value){
+                        if (value == null || value.isEmpty) {
+                          return "password is required!";
+                        }
+                        return null;
+                      },
+                      controller: _passwordController,
+                      decoration: const InputDecoration(label: Text("Password")) ,
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 25,),
+                    /* sign in button */
+                    _signInLoading ? const Center(child: CircularProgressIndicator(),)  : ElevatedButton(onPressed: () async {
+                      final isValid = _formKey.currentState?.validate();
+                      if(isValid != true){
+                        return;
+                      }
+                      setState(() {
+                        _signInLoading = true;
+                      });
+                      try {
+                        await supabase.auth.signInWithPassword(email: _emailController.text, password: _passwordController.text);                        
+                      } catch (e) {
+                        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Sign In failed! "), backgroundColor: Colors.red,));
+                        setState(() {
+                          _signInLoading = false;
+                        });
+                      }
+                    }, child: const Text("Sign In")),
+                    
+                    /* sign up button */
+                    _signUpLoading ? const Center(child: CircularProgressIndicator(),) : OutlinedButton(onPressed: () async {
+                      final isValid = _formKey.currentState?.validate();
+                      if(isValid != true){
+                        return;
+                      }
+                      setState(() {
+                        _signUpLoading = true;
+                      });
+                      try {
+                        await supabase.auth.signUp(email: _emailController.text, password: _passwordController.text);
+                        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Success! confirmation email sent!"), backgroundColor: Colors.green,));
+                        setState(() {
+                          _signUpLoading = false;
+                        });
+                      } catch (e) {
+                        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Sign up failed! confirmation email sent!"), backgroundColor: Colors.red,));
+                        setState(() {
+                          _signUpLoading = false;
+                        });
+                      }
+                    }, 
+                    child: const Text("Sign Up")),
+                    const Row(
+                      children: [
+                        Expanded(child: Divider()),
+                        Padding(padding: EdgeInsets.all(15), child: Text("Or"),),
+                        Expanded(child: Divider()),                     
+                        ],
+                    ),
+                    _googleSignInLoading ? const Center(child: CircularProgressIndicator(),) : OutlinedButton.icon(onPressed: () async {
+                      setState(() {
+                        _googleSignInLoading = true;                        
+                      });
+                      try {
+                        await supabase.auth.signInWithOAuth(OAuthProvider.google, redirectTo: kIsWeb ? null : 'io.supabase.myflutterapp://login-callback/');
+                      } catch (e) {
+                        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("google signup failed!"), backgroundColor: Colors.red,));
+                        setState(() {
+                          _googleSignInLoading = false;
+                        });
+                      }
+                    }, icon: Image.network("https://clipartcraft.com/images/google-logo-png.png", height: 20,),  label: const Text("continue with Google"))
                   ],
                 ),
               )),
