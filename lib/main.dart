f55817670e1ee6ba0b06bd41f6206487574cac69
hanyusok <hanyusok@gmail.com>
2024-06-17 18:00:36 +0900

    init

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_supabase/pages/home_page.dart';
+import 'package:flutter_supabase/pages/start_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
+  String supabaseUrl = dotenv.env['SUPABASE_URL'] ?? '';
+  String supabaseKey = dotenv.env['SUPABASE_KEY'] ?? '';
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Supabase',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const AuthPage(),
+    );
+  }
+}
+
+class AuthPage extends StatefulWidget {
+  const AuthPage({super.key});
+
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  final SupabaseClient supabase = Supabase.instance.client;
+  User? _user;
+
+  @override
+  void initState() {
+    _getAuth();
+    super.initState();
+  }
+
+  Future<void> _getAuth() async {
+    setState(() {
+      _user = supabase.auth.currentUser;
+    });
+    supabase.auth.onAuthStateChange.listen((event) {
+      setState(() {
+        _user = event.session?.user;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _user == null ? const StartPage() : const HomePage();
+  }
+}
---
+++ lib/pages/start_page.dart
@@ -0,0 +1,49 @@
+import "package:flutter/material.dart";
+import "package:supabase_flutter/supabase_flutter.dart";
+
+class StartPage extends StatefulWidget {
+  const StartPage({super.key});
+
+  @override
+  State<StartPage> createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  final SupabaseClient supabase = Supabase.instance.client;
+  bool _signInLoading = false;
+  bool _signUpLoading = false;
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    supabase.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Form(
+              key: _formKey,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Image.network(
+                        "https://seeklogo.com/images/S/supabase-logo-DCC676FFE2-seeklogo.com.png")
+                  ],
+                ),
+              )),
+        ),
+      ),
+    );
+  }
+}
